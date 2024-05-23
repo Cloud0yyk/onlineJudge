ca22b206dcf193cbf713dae0d9c9d379f8760fcc
Cloud0yyk <1853231597@qq.com>
2024-05-23 10:34:50 +0800

    5.23

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,18 @@
 import BasicLayout from "@/layouts/BasicLayout.vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { onMounted } from "vue";
+
+/**
+ * 全局初始化函数，有全局单次调用的代码，都可以写在这里
+ */
+const doInit = () => {
+  console.log("hello");
+};
+
+onMounted(() => {
+  doInit();
+});
 
 const router = useRouter();
 const store = useStore();
---
+++ src/views/NoAuthView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">you don't have authority!.</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NoAuthView",
+});
+</script>
