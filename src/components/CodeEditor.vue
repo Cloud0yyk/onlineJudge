cf8f24c6952e98c16d38ab688cdef1c3b00cd960
Cloud0yyk <1853231597@qq.com>
2024-06-03 23:51:45 +0800

    5.26 night

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="code-editor" ref="codeEditorRef" style="min-height: 400px" />
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, toRaw, withDefaults, defineProps } from "vue";
+import * as monaco from "monaco-editor";
+
+const codeEditorRef = ref();
+const codeEditor = ref();
+
+/**
+ * 定义组件属性类型
+ */
+interface Props {
+  value: string;
+  handleChange: (v: string) => void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+
+onMounted(() => {
+  if (!codeEditorRef.value) return;
+
+  // Hover on each property to see its docs!
+  codeEditor.value = monaco.editor.create(codeEditorRef.value, {
+    value: props.value,
+    language: "java",
+    automaticLayout: true,
+    minimap: {
+      enabled: true,
+    },
+    // lineNumbers: "off",
+    // roundedSelection: false,
+    // scrollBeyondLastLine: false,
+    colorDecorators: true,
+    readOnly: false,
+    theme: "vs-dark",
+  });
+
+  codeEditor.value.onDidchangeModelContent(() => {
+    console.log("目前内容为:", toRaw(codeEditor.value).getValue());
+    props.handleChange(toRaw(codeEditor.value).getValue());
+  });
+});
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <MDEditor :value="value" :handle-change="onChange" />
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!--    <MDEditor :value="value" :handle-change="onChange" />-->
+    <CodeEditor :value="value" :handle-change="onChange" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
-import MDEditor from "@/components/MDEditor.vue"; // @ is an alias to /src
+import MDEditor from "@/components/MDEditor.vue";
+import CodeEditor from "@/components/CodeEditor.vue"; // @ is an alias to /src
 
-const value = ref("");
+const value = ref("1563135");
 const onChange = (v: string) => {
   value.value = v;
+  console.log(v);
+  console.log("hello");
 };
 </script>
